345ff1215f2c5d26552a2fe844f7f261894b8462
Bosch <boschio91@gmail.com>
2022-06-28 15:47:58 -0400

    SQL scripts created. Config and Env Variables setup.

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { PORT } = require("./config")
 
 const { BadRequestError, NotFoundError } = require("./utils/errors")
 
@@ -27,8 +28,6 @@ app.use((error, req, res, next) => {
         error: { message, status },
     })
 })
-
-const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`🐱‍👤Server running http://localhost:${PORT}`)
